d964ad6974c9371375d1d592b7bd5548a21f4a92
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-09-08 12:07:15 +0600

    store updated for Query

--- accuweather/src/redux/store.js
+++ accuweather/src/redux/store.js
@@ -1,16 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { weatherApi } from "./slices/weatherSlice"; // Use named import
 import { hourlyWeatherApi } from "./slices/hourlyWeatherSlice";
 import { dailyWeatherApi } from "./slices/dailyWeatherSlice";
-import weatherReducer from "./slices/weatherSlice";
 
 const store = configureStore({
   reducer: {
-    weather: weatherReducer,
+    [weatherApi.reducerPath]: weatherApi.reducer, // Use the named reducer from weatherApi
     [hourlyWeatherApi.reducerPath]: hourlyWeatherApi.reducer,
     [dailyWeatherApi.reducerPath]: dailyWeatherApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
+      weatherApi.middleware, // Add RTK Query middleware for weather API
       hourlyWeatherApi.middleware,
       dailyWeatherApi.middleware
     ),
